d8406b6789042967a872a38bc63b89162e5724ff
rahim einollahi <r.einollahi@gamil.com>
2022-05-07 17:14:17 +0430

    project is doen

---
+++ src/components/HomePage/DataAnalytics/DataRightside.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class=" ">
+        <span><i class="fa fa-file"></i></span>
+        <div class="text-start">
+            <h4 class="text-info">Fiver</h4>
+            <h6>Create your account today</h6>
+            <hr>
+            <h5>Data integration in minutes, not days</h5>
+        </div>
+
+        <div class="mt-5">
+            <i class="fa fa-home"></i>
+            <span class="display-6">14-day enterprise platform trial</span>
+        </div>
+        <div class="mt-5">
+            <i class="fa fa-car"></i>
+            <span class="display-6">No coding required</span>
+        </div>
+        <div class="mt-5">
+            <i class="fa fa-drive"></i>
+            <span class="display-6">14-day enterprise platform trial</span>
+        </div>  
+        <form action="" class="">
+            <div class="">
+                <label for="txt-fName" class="form-label d-block text-start"> firstName</label>
+                <input type="text" class="form-control w-50" id="txt-fName" name="txt-fName">
+            </div>
+            <div class="">
+                <label for="txt-email" class="form-label d-block text-start"> firstName</label>
+                <input type="email" class="form-control w-50" id="txt-email" name="txt-email">
+            </div>
+
+            <div class="">
+                <label for="txt-lName" class="form-label d-block text-start"> lastName</label>
+                <input type="text" class="form-control w-50" id="txt-lName" name="txt-flName">
+            </div>
+
+            <div class="">
+                <label for="txt-fName" class="form-label d-block text-start"> firstName</label>
+                <input type="text" class="form-control w-50" id="txt-fName" name="txt-fName">
+            </div>
+
+                <input type="submit" class="btn btn-info w-50 m-3 p-2" value="signin">
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/DataAnalytics.vue
+++ src/views/DataAnalytics.vue
@@ -4,15 +4,18 @@
             <div class="col-md-6">
                 <data-leftside />
             </div>
-            <div class="col-md-6"></div>
+            <div class="col-md-6">
+                <data-rightside />
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 import DataLeftside from '../components/HomePage/DataAnalytics/DataLeftside.vue'
+import DataRightside from '../components/HomePage/DataAnalytics/DataRightside.vue'
     export default {
-  components: { DataLeftside },
+  components: { DataLeftside, DataRightside },
         
     }
 </script>
